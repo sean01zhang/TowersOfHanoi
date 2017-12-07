9c6deb3a5dd43d8ae040a95da0a446520fecccc9
Sean Zhang <seanzhang2001@gmail.com>
2017-12-06 23:07:37 -0500

    Done hanoi method that solves the towers, given a starting stack size

--- src/towersofhanoi/TowersOfHanoi.java
+++ src/towersofhanoi/TowersOfHanoi.java
@@ -5,6 +5,8 @@
  */
 package towersofhanoi;
 
+import java.util.Scanner;
+
 /**
  *
  * @author seanzhang
@@ -15,16 +17,25 @@ public class TowersOfHanoi {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        hanoi(2,3,1,2);
+        Scanner input = new Scanner(System.in);
+        
+        int x = Math.abs(input.nextInt());
+        
+        hanoi(x,3,1,2);
     }
     
+    
     public static void hanoi(int level, int to, int from, int placeholder) {
-        if(level < 1) {
+        //base case: if the tower only has one disc, move it from its current position to the position in interest.
+        if(level <= 1) {
+            //prompt user
             System.out.println("Move " + from + " to " + to);
         } else {
-            hanoi(level-1,from,to,placeholder);
+            //move the disks above the lowest disk to the temporary placeholder stack
+            hanoi((level-1),placeholder,from,to);
             System.out.println("Move " + from + " to " + to);
-            hanoi(level-1, placeholder, from, to);
+            //move the disks from the temporary placeholder stack to the end stack
+            hanoi(level-1, to, placeholder, from);
 
         }
     }
